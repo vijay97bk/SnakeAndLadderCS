dd77977ee532f07d678c26d978b037673a2ad22d
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 13:20:54 +0530

    [vijay]AddedWinningPosition100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,17 +7,27 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             int POSITION = 0;
+            
             Random random = new Random();
+            while (POSITION < 100)
+                { 
             int DiceValue = random.Next(1, 6);
             int Choice = random.Next(0, 2);
-            {     
-            if (Choice == 0)
-              Console.WriteLine("No Play");
-             else if (Choice == 1)
+
+                if (Choice == 0)
+                    Console.WriteLine("No Play");
+                else if (Choice == 1)
+                {
                     POSITION += DiceValue;
-             else
-                POSITION -= DiceValue;
-            }        
+                    Console.WriteLine("Ladder" + POSITION);
+
+                }
+                else
+                {
+                    POSITION -= DiceValue;
+                    Console.WriteLine("Snake" + POSITION);
+                }   
+                }   
 
         }
     }
