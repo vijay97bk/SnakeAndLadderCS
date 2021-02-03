6fa7cdfd33a7cc56cc04b4c7f89c92807b13e46f
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 16:01:16 +0530

    [vijay]Added Player2&Winner

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,40 +13,81 @@ namespace SnakeAndLadder
             
             Random random = new Random();
             int Turn = random.Next(0, 1);
-
-            while (POSITION_PLAYER_1 < FINAL_POSITION)
-            { 
-            int DiceValue = random.Next(1, 6);
-            int Choice = random.Next(0, 2);
-
-                switch (Choice)
+            int PlayerTurn = random.Next(0, 1);
+            while (POSITION_PLAYER_1 < FINAL_POSITION && POSITION_PLAYER_2 < FINAL_POSITION)
+            {
+                if (PlayerTurn == 0)
                 {
-                    case 0:
-                        Console.WriteLine("No Play");
-                        break;
 
-                    case 1:
-                        POSITION_PLAYER_1 += DiceValue;
-                        Console.WriteLine("Ladder" + POSITION_PLAYER_1);
-                        if (POSITION_PLAYER_1 > 100)
-                        {
-                            POSITION_PLAYER_1 -= DiceValue;
-                        }
-                        break;
+                    int DiceValuePlayer1 = random.Next(1, 6);
+                    int Choice = random.Next(0, 2);
 
-                    default:
-                        POSITION_PLAYER_1 -= DiceValue;
-                        Console.WriteLine("Snake" + POSITION_PLAYER_1);
-                        if (POSITION_PLAYER_1 < 0)
-                        {
-                            POSITION_PLAYER_1 = 0;
-                        }
-                        break;
+                    switch (Choice)
+                    {
+                        case 0:
+                            Console.WriteLine("No Play");
+                            break;
+
+                        case 1:
+                            POSITION_PLAYER_1 += DiceValuePlayer1;
+                            Console.WriteLine("Ladder" + POSITION_PLAYER_1);
+                            if (POSITION_PLAYER_1 > 100)
+                            {
+                                POSITION_PLAYER_1 -= DiceValuePlayer1;
+                            }
+                            break;
+
+                        default:
+                            POSITION_PLAYER_1 -= DiceValuePlayer1;
+                            Console.WriteLine("Snake" + POSITION_PLAYER_1);
+                            if (POSITION_PLAYER_1 < 0)
+                            {
+                                POSITION_PLAYER_1 = 0;
+                            }
+                            break;
+                    }
+                    NumberOfTimesDiceRolled++;
+                    Console.WriteLine("Number of times dice rolled: " + NumberOfTimesDiceRolled);
+
                 }
-                NumberOfTimesDiceRolled++;
-                Console.WriteLine("Number of times dice rolled: " + NumberOfTimesDiceRolled);
-            }   
 
+                else
+                {
+                    int DiceValuePlayer2 = random.Next(1, 6);
+                    int Choice = random.Next(0, 2);
+                    switch (Choice)
+                    {
+                        case 0:
+                            Console.WriteLine("No Play");
+                            break;
+
+                        case 1:
+                            POSITION_PLAYER_1 += DiceValuePlayer2;
+                            Console.WriteLine("Ladder" + POSITION_PLAYER_1);
+                            if (POSITION_PLAYER_1 > 100)
+                            {
+                                POSITION_PLAYER_1 -= DiceValuePlayer2;
+                            }
+                            break;
+
+                        default:
+                            POSITION_PLAYER_1 -= DiceValuePlayer2;
+                            Console.WriteLine("Snake" + POSITION_PLAYER_1);
+                            if (POSITION_PLAYER_1 < 0)
+                            {
+                                POSITION_PLAYER_1 = 0;
+                            }
+                            break;
+                    }
+                    NumberOfTimesDiceRolled++;
+                    Console.WriteLine("Number of times dice rolled: " + NumberOfTimesDiceRolled);
+                }
+
+            }
+            if (POSITION_PLAYER_1 == FINAL_POSITION)
+                Console.WriteLine("Player 1 wins");
+            else
+                Console.WriteLine("Player 2 wins");
         }
     }
 }
