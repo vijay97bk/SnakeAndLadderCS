95777a7057e8e5efb1a2da891b47c8a45f56e878
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-03 13:05:01 +0530

    [vijay]Added OptionToNoplaySnakeLadder

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,6 +9,15 @@ namespace SnakeAndLadder
             int POSITION = 0;
             Random random = new Random();
             int DiceValue = random.Next(1, 6);
+            int Choice = random.Next(0, 2);
+            {     
+            if (Choice == 0)
+              Console.WriteLine("No Play");
+             else if (Choice == 1)
+                    POSITION += DiceValue;
+             else
+                POSITION -= DiceValue;
+            }        
 
         }
     }
